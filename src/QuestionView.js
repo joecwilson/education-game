a95864e46ee47e6667d2c30bf032d4028fbf6151
joseph <personalwcj0715@gmail.com>
2022-07-20 14:04:27 -0500

    QUestion View is now Used

--- src/QuestionView.js
+++ src/QuestionView.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function QuestionView(){
     return(
-        <div className="Question">
-            <div className="field">
+        <div className="container mx-auto bg-slate-400 rounded-md m-8 ">
+            <div className="m-4 text-center">
                 <span>This is the text of the question</span>
             </div>
-            <form className="from-orange-100">
+            <form className="from-orange-500">
                 <label for="ans"> Answer</label>
-                <input type="text" id="ans" name="ans" value="0"></input>
+                <input type="number" id="ans" name="ans" value="0"></input>
             </form>
         </div>
     )
